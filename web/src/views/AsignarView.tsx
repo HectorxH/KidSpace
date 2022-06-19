3f529f2ae6bd0726afed6d3c8ba7c23904f8cec5
HectorxH <HectorPLJ@gmail.com>
2022-06-18 21:46:19 -0400

    Remove console.log :(

--- web/src/views/AsignarView.tsx
+++ web/src/views/AsignarView.tsx
@@ -46,8 +46,6 @@ const AsignarView = () => {
     setTimeout(handleBack, 1200);
   };
 
-  console.log(localStorage.getItem('asignadas'));
-
   return (
     <Box sx={{
       px: 4, py: 2, mt: 2, width: { sx: '100%', md: '70%' },
