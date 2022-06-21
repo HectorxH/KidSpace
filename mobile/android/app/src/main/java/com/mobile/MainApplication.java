24f8f7a450f9cb9d526eaff7b1554a43817e7fe7
Ignacio Jorquera <ignacio.jorquera@sansano.usm.cl>
2022-06-20 22:36:28 -0400

    Viro env

--- mobile/android/app/src/main/java/com/mobile/MainApplication.java
+++ mobile/android/app/src/main/java/com/mobile/MainApplication.java
@@ -12,6 +12,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.viromedia.bridge.ReactViroPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -28,6 +29,8 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+          // Viro cosas
+          packages.add(new ReactViroPackage(ReactViroPackage.ViroPlatform.valueOf("AR")));
           return packages;
         }
 
