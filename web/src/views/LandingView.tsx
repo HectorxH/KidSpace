7dc709497f87b96ac13d9b0cf314fc8fbf40bd8b
tletelier <72598467+tletelier@users.noreply.github.com>
2022-10-18 23:33:42 -0300

    kinda responsive landing page

--- web/src/views/LandingView.tsx
+++ web/src/views/LandingView.tsx
@@ -3,10 +3,14 @@
 /* eslint-disable no-underscore-dangle */
 import React, { useEffect, useState } from 'react';
 import {
-  AppBar, Button, Stack, Theme, Typography, Toolbar, Box, Card, SvgIcon,
+  AppBar, Button, Stack, Theme, Typography,
+  Toolbar, Box, Card, SvgIcon, Grid, CardMedia, MenuItem,
+  Menu, Container, IconButton,
 } from '@mui/material';
 import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import AdbIcon from '@mui/icons-material/Adb';
+import MenuIcon from '@mui/icons-material/Menu';
 import { faGamepad, faFaceSmile, faVrCardboard } from '@fortawesome/free-solid-svg-icons';
 import ReactPlayer from 'react-player/youtube';
 import PupilosTable from '../components/PupilosTable';
@@ -21,47 +25,106 @@ import features from '../mock/features';
 const imgLogo = require('../assets/logo-horizontal.png');
 const imgLanding = require('../assets/landing.png');
 
-const LandingView = () => {
-  const [estudiantes, setEstudiantes] = useState<IEstudiantes>();
-  const [loading, setLoading] = useState(true);
+const pages = ['Iniciar Sesión', 'Registro'];
+
+const ResponsiveAppBar = () => {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
 
   return (
-    <>
-      <AppBar
-        position="static"
-        sx={{
-          backgroundColor: '#FFF',
-        }}
-      >
-        <Stack direction="row" sx={{ width: 1 }}>
-          <Toolbar sx={{ justifyContent: 'flex-start' }}>
-            <Box
-              component="img"
-              src={imgLogo}
-              alt="logo"
-              sx={{
-                height: 64,
-              }}
-            />
-          </Toolbar>
-          <Toolbar sx={{ justifyContent: 'flex-end' }}>
-
-            <Button sx={{
-              backgroundColor: '#FFF', borderRadius: 5, margin: 2, textTransform: 'none', alignSelf: 'center',
+    <AppBar
+      position="static"
+      sx={{
+        backgroundColor: '#FFF',
+      }}
+    >
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Box
+            component="img"
+            src={imgLogo}
+            alt="logo"
+            sx={{
+              height: 70,
             }}
+          />
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' }, justifyContent: 'flex-end' }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              // color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'flex-end' }}>
+            <Button
+              onClick={handleCloseNavMenu}
+              sx={{
+                my: 2,
+                mx: 2,
+                backgroundColor: '#FFF',
+                textTransform: 'none',
+                borderRadius: 5,
+                display: 'block',
+                minWidth: 150,
+                alignSelf: 'center',
+              }}
             >
               <Typography>
                 Iniciar Sesión
               </Typography>
             </Button>
             <Button
+              onClick={handleCloseNavMenu}
               sx={{
+                my: 2,
                 backgroundColor: (theme: Theme) => theme.palette.secondary.main,
                 borderRadius: 5,
-                color: '#FFF',
+                color: (theme: Theme) => theme.palette.primary.contrastText,
                 textTransform: 'none',
                 minWidth: 150,
                 alignSelf: 'center',
+                display: 'block',
+                '&:hover': {
+                  backgroundColor: '#ffbe82',
+                  color: '#FFFFFF',
+                },
               }}
             >
               <Typography sx={{
@@ -71,22 +134,34 @@ const LandingView = () => {
                 Registrarme
               </Typography>
             </Button>
-          </Toolbar>
-        </Stack>
-      </AppBar>
-      <Stack sx={{ px: 10, py: 6 }}>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+
+const LandingView = () => {
+  const [estudiantes, setEstudiantes] = useState<IEstudiantes>();
+  const [loading, setLoading] = useState(true);
+  const [winWidth, setWinWidth] = useState<number>(window.innerWidth);
+  console.log(winWidth);
+  return (
+    <>
+      <ResponsiveAppBar />
+      <Stack sx={{ py: 6 }}>
         <Stack
           direction="row"
           sx={{ flexWrap: 'wrap' }}
         >
-          <Stack spacing={3} sx={{ width: 'calc(50%)', minWidth: 400 }}>
+          <Stack spacing={3} sx={{ width: winWidth < 400 ? 1 : '40vw', margin: winWidth < 400 ? 5 : 10 }}>
             <Typography variant="h4" sx={{ color: '#000' }}>
               <b>Aplicación que enseña sobre las profesiones.
                 Derribando inequidades y sesgos que determinan
                 las proyecciones de los niños y las niñas.
               </b>
             </Typography>
-            <Typography sx={{ color: '#000' }}>
+            <Typography variant="h6" sx={{ color: '#000' }}>
               La aplicación está pensada para complementar las
               clases de Tecnología de 6to básico,
               donde por primera vez se habla sobre las profesiones.
@@ -97,6 +172,10 @@ const LandingView = () => {
                 borderRadius: 4,
                 color: '#FFF',
                 textTransform: 'none',
+                '&:hover': {
+                  backgroundColor: '#ffbe82',
+                  color: '#FFFFFF',
+                },
                 maxWidth: 150,
               }}
             >
@@ -108,14 +187,14 @@ const LandingView = () => {
               </Typography>
             </Button>
           </Stack>
-          <Stack sx={{ width: 0.5, minWidth: 400 }}>
+          <Stack sx={{ width: winWidth < 400 ? 1 : '40vw' }}>
             <Box
               component="img"
               src={imgLanding}
               alt="logo"
               sx={{
                 alignSelf: 'center',
-                height: 400,
+                width: 1,
               }}
             />
           </Stack>
@@ -174,9 +253,11 @@ const LandingView = () => {
                     />
                   )}
               </Stack>
-              <Typography sx={{
-                color: '#000', margin: 1, alignSelf: 'center', fontSize: 18,
-              }}
+              <Typography
+                align="center"
+                sx={{
+                  color: '#000', margin: 1, alignSelf: 'center', fontSize: 18,
+                }}
               >
                 <b>{feature.title}</b>
               </Typography>
@@ -196,7 +277,7 @@ const LandingView = () => {
             maxWidth: 500, alignItems: 'center',
           }}
           >
-            <Typography variant="h6" sx={{ color: '#000', margin: 1 }}>
+            <Typography align="center" variant="h6" sx={{ color: '#000', margin: 1 }}>
               <b>Elige un plan</b>
             </Typography>
             <Typography sx={{ color: '#000' }} align="center">
@@ -233,7 +314,7 @@ const LandingView = () => {
                   <b>{paquete.title}</b>
                 </Typography>
               </Stack>
-              <Typography variant="h5" sx={{ color: '#000', alignSelf: 'center' }}>
+              <Typography align="center" variant="h5" sx={{ color: '#000', alignSelf: 'center' }}>
                 <b>{paquete.price}</b>
               </Typography>
               <Typography sx={{ color: '#000', alignSelf: 'center', margin: 1 }}>
@@ -247,6 +328,10 @@ const LandingView = () => {
                   textTransform: 'none',
                   width: 150,
                   margin: 1,
+                  '&:hover': {
+                    backgroundColor: '#ffbe82',
+                    color: '#FFFFFF',
+                  },
                 }}
               >
                 <Typography sx={{
