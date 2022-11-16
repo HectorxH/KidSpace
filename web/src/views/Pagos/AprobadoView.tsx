7b07c42f7c4c61207ea0f3a35d331c990be54b31
HectorxH <HectorPLJ@gmail.com>
2022-11-16 00:34:03 -0300

    fix fix fix

--- web/src/views/Pagos/AprobadoView.tsx
+++ web/src/views/Pagos/AprobadoView.tsx
@@ -15,11 +15,14 @@ import { useAuth } from '../../hooks/useAuth';
 const Aprobado = () => {
   const { planId } = useParams();
 
-  const { logout, navigateToDefault } = useAuth();
+  const {
+    user, login, logout, navigateToDefault,
+  } = useAuth();
 
   const handleClick = async () => {
     try {
       const res = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/Representante/plan`, { plan: planId });
+      login(user, planId);
       console.log(res);
     } catch (e) {
       console.log(e);
