5afe2367f0c3fbf3d03392ea18918e7bd1a1e8a2
HectorxH <HectorPLJ@gmail.com>
2022-11-16 17:46:30 -0300

    fix

--- web/src/views/institucion/EstadisticasProfesorInstitucionView.tsx
+++ web/src/views/institucion/EstadisticasProfesorInstitucionView.tsx
@@ -182,7 +182,7 @@ const EstadisticasProfesorInstitucionView = () => {
       setCountCorrectas(res.data.countCorrectas);
       res = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/Estadisticas/profesor/${profesorId}/%curso`, { dateRange });
       setActividadesCurso(res.data.actividadesCurso);
-      res = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/Estadisticas//profesor/${profesorId}/historialDocente`, { dateRange });
+      res = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/Estadisticas/profesor/${profesorId}/historialDocente`, { dateRange });
       setHistorial(res.data.historial);
     } catch (e) {
       console.log(e);
